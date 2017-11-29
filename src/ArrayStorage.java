dd88f65dab48e5b6703bcb543e103b676e5ff9fc
Serg <ablaev.sergey@gmail.com>
2017-11-29 12:20:02 +0500

    урок1 рефакторинг

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,13 +2,14 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[100];  // 10000 пока уменьшил )
+    private Resume[] storage = new Resume[10000];
 
     //добавлю переменную - в которой буду хранить колич значимых элементов в массиве (храниим от 0 до count)
     private int count = 0;  // меняем только из методов!
 
     void clear() {
-    count = 0; //тут просто )
+        for (int i=0;i<count;i++) storage[i] = null;
+        count = 0;
     }
 
     void save(Resume r) {
@@ -18,8 +19,8 @@ public class ArrayStorage {
 
     private int getInt(String uuid) {
         //ищем знач массива, если нет - вернем минус один ?
-        for (int i=0; i<count; i++){
-            if (storage[i].uuid==uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid == uuid) {
                 return i;
             }
         }
@@ -29,27 +30,16 @@ public class ArrayStorage {
     Resume get(String uuid) {
         Resume foundElement;
         int i = getInt(uuid);
-        if (i == -1) {
-            foundElement = null;   // элемент не найден
-        } else {
-            foundElement = storage[i];
-        }
-        return foundElement;
+        return (i==-1)?null:storage[i];
     }
 
     void delete(String uuid) {
-        // при удалении элемента сдвигаем все элементы от текущего до 'count'
+        // при удалении элемента заменяем стираемую ссылку элементом с вершины данных
         int index = getInt(uuid);
         if (index == -1) {
             // элемент не найден
         } else {
-            storage[index]=null; //удаляем: ВОПРОС - стоит ли так явно прописывать стирание элем. для удобства понимания программы?
-                                 // ведь нииже я все равно затру эту ячейку при уплотнении
-            // начинаем переносить элементы из i+1 в i пока i+1<=count !!
-            for (int i=index; i<count; i++){
-                storage[i]=storage[i+1];
-            }
-            storage[count] = null;
+            storage[index] = storage[count-1];
             --count;
         }
     }
@@ -59,10 +49,7 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] returnResume = new Resume[count];   // ВОПРОС при каждом вызове создавать массив через new - правильно?
-/*
-        for (int i=0; i<count; i++) { returnResume[i]=storage[i]; }  //я написал так но умная IDEA предложила метод arraycopy (ниже)
- */
-        System.arraycopy(storage, 0, returnResume, 0, count);  // ВОПРОС! этот метод быстрее ручного копирования?
+        System.arraycopy(storage, 0, returnResume, 0, count);
         return returnResume;
     }
 
