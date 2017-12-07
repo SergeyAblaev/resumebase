7105865c5ce86c60b85df1df775d0ed865f83eb9
Serg <ablaev.sergey@gmail.com>
2017-12-08 01:15:49 +0500

    HW04

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -1,3 +1,4 @@
+
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
@@ -24,8 +25,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
+        Resume searchKey = new Resume(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 }
