184cb990ffc7fbdbd4bd7763603c7c9d7f97422e
Serg <ablaev.sergey@gmail.com>
2017-12-10 18:27:45 +0500

    АП2 101217

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -16,7 +16,7 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         System.out.println(r);
-        // now invoke r.toString via reflection: АП 101217
+        // now invoke r.toString via reflection: АП 101217 - +2 АП с ноута
         Method method = r.getClass().getDeclaredMethod("toString");
         System.out.println(method.invoke(r));
     }
