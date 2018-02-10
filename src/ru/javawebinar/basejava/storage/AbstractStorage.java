807d7c8265ffcbf22bba91eaf5857928a9c80f3e
SergeyAblaev <ablaev.sergey@gmail.com>
2018-02-11 02:49:40 +0500

    HW06 version 11/02/2018

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -10,13 +10,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public abstract class AbstractStorage implements Storage {
-    protected static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
-        @Override
-        public int compare(Resume o1, Resume o2) {
- //         return o1.getUuid().compareTo(o2.getUuid());
-            return o1.getFullName().compareTo(o2.getFullName());
-        }
-    };
+    protected static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getFullName().compareTo(o2.getFullName());
 
     protected abstract Object getSearchKey(String uuid);
 
